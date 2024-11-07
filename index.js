a91cc4e48317c917fea33d7705aa7711f5931dd1
maleahy <maleahyee@gmail.com>
2024-11-07 19:53:36 +0000

    updating index

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
 let express = require("express");
-let app = new express();                                                                     
+let app = new express();
+
+console.log('hello world');
 
 // set up database connection
-const knex = require("knex")({
- client: "mysql",
- connection: {
-  host:"concert-db-instance-1.c61dq6ysma4i.us-east-2.rds.amazonaws.com",
-  user: "admin",
-  password: "Password1",
-  database:"paradise-concerts",
-  port: 3306,
+const knex = require("knex")({ client: "mysql", connection: {
+  host:"myrdsinstance.c5umum2q4xak.us-east-2.rds.amazonaws.com", 
+  user: "admin", password: "Password1", database:"crocs", port: 
+  3306,
  },
 });
 
-app.get("/",(req,res) => {
- knex
- .select()
- .from("venues")
+app.get("/",(req,res) => { knex .select() .from("CrocsShoes") 
  .then((result) => {
-  console.log(result);
-  res.send(result);
- }); 
+  console.log(result); res.send(result);
+ });
 });
+
+console.log('Server is listening on port 3000');
 app.listen(3000);
+
